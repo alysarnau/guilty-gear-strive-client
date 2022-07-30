b1acd2c91c0737639bb8e73bf0e9dab72b111743
Alys <alice.g.cooper@gmail.com>
2022-07-30 10:29:11 -0400

    Home page now renders CharactersIndex

--- guilty-gear-strive-client/src/components/Home.js
+++ guilty-gear-strive-client/src/components/Home.js
@@ -1,10 +1,16 @@
+import CharactersIndex from "./characters/CharactersIndex"
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
+	const { msgAlert } = props
 	return (
 		<>
 			<h2>Home Page</h2>
+			<CharactersIndex
+				msgAlert={ msgAlert }
+			/>
 		</>
 	)
 }
